b2b17a77c6e4b85f7c501155204f0ba1b3a1210c
talk2deepak21 <talk2deepak21@gmail.com>
2019-06-18 08:32:55 +0530

    Update code.ts

--- src/code.ts
+++ src/code.ts
@@ -2,10 +2,28 @@ import { Observable } from 'rxjs';
 
 
 var observable = Observable.create((observer: any)=>{
-    observer.next('Hey guys !!')
+    try{
+        observer.next('Hey guys !!');
+        observer.next('Hey next guys !!');
+        let interval = 0;
+        var intervalFunction = setInterval(()=>{
+            observer.next(`interval ${interval} in second`);
+            interval+= 2;
+            // if(interval > 10)
+            // clearInterval(intervalFunction);
+        }, 2000);
+        //intervalFunction;
+
+    }
+    catch(err){
+        observer.error(err)
+    }
 });
 
-observable.subscribe((x: any)=>{ addItem(x)})
+var observer = observable.subscribe((x: any)=>{ addItem(x)});
+setTimeout(() => {
+    observer.unsubscribe();
+}, 6500);
 
 function addItem(val: any){
     var node = document.createElement('li');
