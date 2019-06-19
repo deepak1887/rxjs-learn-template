5d1f0d39cfdb4cb9d6c895680151085d0c385d92
talk2deepak21 <talk2deepak21@gmail.com>
2019-06-19 23:20:58 +0530

    observer

---
+++ server/data.ts
@@ -0,0 +1,34 @@
+export const allReaders = [
+    {
+        readerId: 1,
+        name: "Marie",
+        weekelyReadingGoals: 400,
+        totalMnutesReaded: 5600
+    },
+    {
+        readerId: 2,
+        name: "Daniel",
+        weekelyReadingGoals: 600,
+        totalMnutesReaded: 5200
+    },
+    {
+        readerId: 3,
+        name: "Lanier",
+        weekelyReadingGoals: 800,
+        totalMnutesReaded: 4600
+    },
+    {
+        readerId: 4,
+        name: "Toney",
+        weekelyReadingGoals: 900,
+        totalMnutesReaded: 4200
+    }
+];
+export const allBooks = [
+    { bookId: 1, title: 'Goodnight Moon', author: 'Margaret Wise Brown', publicationYear: 1992 },
+    { bookId: 2, title: 'An Obedient Father', author: 'Akhil Sharma', publicationYear: 1982 },
+    { bookId: 3, title: 'Count of Monte Cristo', author: 'Alexander Dumas', publicationYear: 1998 },
+    { bookId: 4, title: 'One Day in the Life of Ivan Denisovitch', author: 'Alexander Solzhenitsyn', publicationYear: 1999 },
+    { bookId: 5, title: 'Development as Freedom', author: 'Amartya Sen', publicationYear: 2000 },
+    { bookId: 6, title: 'Chabibandha Sinduk', author: 'Ashapurna Devi', publicationYear: 2002 }
+]
--- src/code.ts
+++ src/code.ts
@@ -1,16 +1,54 @@
-import { Observable } from 'rxjs';
+import { Observable, fromEvent, observable } from 'rxjs';
+import * as data from '../server/data';
+var counter = 0;
 
-
-var observable = Observable.create((observer: any)=>{
-    observer.next('Hey guys !!')
+var observable1 = Observable.create((observer: any)=>{
+    data.allBooks.forEach(book=> { observer.next(book.title)});
+    observer.complete('process complete');
+    //observer.complete('process complete');
+    //observer.next(data.allBooks.map(x=>x.author))
 });
 
-observable.subscribe((x: any)=>{ addItem(x)})
+var observable2 = new Observable((observer: any)=>{
+    observer.next('hello')
+});
+
+var dateObserver = Observable.create((observer: any)=>{
+    counter = 0;
+    var intval = setInterval(()=>{
+        observer.next(new Date());
+        counter++;
+        if(counter>5)
+        clearInterval(intval);
+    }, 1000)
+    
+})
+var interval = (observer: any)=> setInterval(()=>{
+    observer.next(new Date());
+    counter++;
+}, 1000)
+
+dateObserver.subscribe((x: any)=>{ addItem(x)})
+
+setTimeout(()=>{dateObserver.unsubscribe()}, 10000)
 
 function addItem(val: any){
     var node = document.createElement('li');
+    node.className = 'col-md-4';
     var textnode = document.createTextNode(val);
     node.appendChild(textnode);
     document.getElementById('output').appendChild(node);
 }
+function addDate(val: any){
+    var node = document.createElement('li');
+    node.className = 'col-md-4';
+    var textnode = document.createTextNode(val);
+    node.appendChild(textnode);
+    document.getElementById('output').appendChild(node);
+}
+
+// let button = document.getElementById('btnButton');
+// fromEvent(button, 'click').subscribe(
+//     event=>{addItem(event)}
+// )
 //designcourse tutorial
